82f2d1bf39ffa62e729a3e1522a1e9f12ec3e5f5
durini309 <jcdurini@gmail.com>
2016-04-03 21:24:54 -0600

    New Packages
    This version has the latest package for all the solutions.

--- Programa/F_Gudvis/F_Gudvis/F_Gudvis.Droid/Resources/Resource.Designer.cs
+++ Programa/F_Gudvis/F_Gudvis/F_Gudvis.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace F_Gudvis.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::F_Gudvis.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
